1175995979bc351eb7a826e148a5c94f3955f54b
KD <31553720+KDVilla@users.noreply.github.com>
2023-08-08 16:53:27 -0700

    Update tag.ino

--- tag/tag.ino
+++ tag/tag.ino
@@ -27,13 +27,12 @@ void setup() {
   //nfc/rfid
   Serial.begin(115200);
   nfc.begin();
-  unit8_t it[]={0x##, 0x##, 0x##, 0x##};    //it
-  unit8_t self[]={0x##, 0x##, 0x##, 0x##}   //self-player
-  unit8_t other[]={0x##, 0x##, 0x##, 0x##}  //other-player
+  uint8_t it[]={0x##, 0x##, 0x##, 0x##};    //it
+  uint8_t self[]={0x##, 0x##, 0x##, 0x##};   //self-player
+  uint8_t other[]={0x##, 0x##, 0x##, 0x##};  //other-player
 
 
   //lights
-  pinMode (LED_PIN, OUTPUT);
   strip.begin();
   strip.show();
   strip.setBrightness(50);
@@ -42,8 +41,8 @@ void setup() {
 
 
 void loop() {
+  //game begin
   //lights
-
   colorWipe(strip.Color(0,255,0),0);     //game start - all solid green
 
   //nfc
@@ -51,46 +50,18 @@ void loop() {
   uint8_t uidLength;
   nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);   //read card
 
-  while uid[]==it[]{                         //while uid = it; do this
+  if (uid[]==it[]){                             //if uid = it; do this
     colorWipe(strip.Color(0,0,255),0);     //change color to blue
     delay(10000);                           //wait 10 seconds
-    colorWipe(led 0-6 purple);   //count down starts; every ten seconds, 7 more lights change to purple
-    colorWipe(led 7-49 blue);   //don't know the code to address each light 
-    delay(10000);
-    colorWipe(led 0-13 purple); 
-    colorWipe(led 14-49 blue);
-    delay(10000);
-    colorWipe(led 0-20 purple); 
-    colorWipe(led 21-49 blue;)
-    delay(10000);
-    colorWipe(led 0-27 purple);
-    colorWipe(led 28-49 blue);
-    delay(10000);
-    colorWipe(led 0-34 purple); 
-    colorWipe(led 35-49 blue);
-    delay(10000);
-    colorWipe(led 0-41 purple); 
-    colorWipe(led 42-49 blue); 
-    delay(10000);
-    colorWipe(strip.Color(255,0,255)0);         //player melted, all purple
-    delay(10000);
+    countDown(strip.Color(255,0,255),strip.Color(0,0,255)); 
   }
 
-  while uid[]==other[]{
-    colorWipe(strip.Color(0,255,0)0);
+  if (uid[]==other[]){
+    colorWipe(strip.Color(0,255,0)0);         //un-freeze, back to green
   }
-  int fill[] = {1, 2, 3, 4, 5, 13, 11, 10, 9, 7, 14, 15, 17, 19, //array of lights for x fill
-              20, 27, 26, 25, 23, 22, 21, 28, 29, 31, 33, 34, 41,
-               39, 38, 37, 35, 43, 44, 45, 46, 47},
-  int x[]={0, 6, 12, 8, 16, 18, 24, 30, 32, 40, 36, 42, 48};    //array of lights for x
 
-  while uid[]==self[] {                       //if you try to cheat&tag yourself
-    colorWipe(fill[] red);                    //lights show big "x"
-    colorWipe(x[] yellow);
-    delay(1000);                              //one second wait
-    colorWipe(x[] red);                       //flip colors
-    colorWipe(fill[] yellow);
-    delay(1000);
+  if (uid[]==self[]) {                                            //if you try to cheat&tag yourself
+    cheat(strip.Color(255,0, 0),strip.Color(255,255,0));        //lights show big "x"
   }                    
 
 }
@@ -101,4 +72,173 @@ void colorWipe(uint32_t color, int wait) {
     strip.show();                          //  Update strip to match
     delay(wait);                           //  Pause for a moment
   }
+}
+
+void countDown(uint32_t color1, uint32_t color2){
+  for(int i=0; i<7; i++){
+    strip.setPixelColor(i, color1);
+  }
+  for(int k=7; k<strip.numPixels(); k++){
+    strip.setPixelColor(k, color2);
+  }
+  strip.show();
+  delay(10000);
+
+  for(int i=0; i<14; i++){
+    strip.setPixelColor(i, color1);
+  }
+  for(int k=14; k<strip.numPixels(); k++){
+    strip.setPixelColor(k, color2);
+  }
+  strip.show();
+  delay(10000);
+
+  for(int i=0; i<21; i++){
+    strip.setPixelColor(i, color1);
+  }
+  for(int k=21; k<strip.numPixels(); k++){
+    strip.setPixelColor(k, color2);
+  }
+  strip.show();
+  delay(10000);
+
+  for(int i=0; i<28; i++){
+    strip.setPixelColor(i, color1);
+  }
+  for(int k=28; k<strip.numPixels(); k++){
+    strip.setPixelColor(k, color2);
+  }
+  strip.show();
+  delay(10000);
+
+  for(int i=0; i<35; i++){
+    strip.setPixelColor(i, color1);
+  }
+  for(int k=35; k<strip.numPixels(); k++){
+    strip.setPixelColor(k, color2);
+  }
+  strip.show();
+  delay(10000);
+
+  for(int i=0; i<42; i++){
+    strip.setPixelColor(i, color1);
+  }
+  for(int k=42; k<strip.numPixels(); k++){
+    strip.setPixelColor(k, color2);
+  }
+  strip.show();
+  delay(10000);
+
+  for(int k=0; k<strip.numPixels(); k++){
+    strip.setPixelColor(k, color2);
+  }
+  strip.show();
+  delay(10000);
+}
+
+void cheat(uint32_t color1, uint32_t color2){
+  strip.setPixelColor(1, color1);       //fill
+  strip.setPixelColor(2, color1);
+  strip.setPixelColor(3, color1);
+  strip.setPixelColor(4, color1);
+  strip.setPixelColor(5, color1);
+  strip.setPixelColor(13, color1);
+  strip.setPixelColor(11, color1);
+  strip.setPixelColor(10, color1);
+  strip.setPixelColor(9, color1);
+  strip.setPixelColor(7, color1);
+  strip.setPixelColor(20, color1);
+  strip.setPixelColor(19, color1);
+  strip.setPixelColor(17, color1);
+  strip.setPixelColor(15, color1);
+  strip.setPixelColor(14, color1);
+  strip.setPixelColor(27, color1);
+  strip.setPixelColor(26, color1);
+  strip.setPixelColor(25, color1);
+  strip.setPixelColor(23, color1);
+  strip.setPixelColor(22, color1);
+  strip.setPixelColor(21, color1);
+  strip.setPixelColor(34, color1);
+  strip.setPixelColor(33, color1);
+  strip.setPixelColor(31, color1);
+  strip.setPixelColor(29, color1);
+  strip.setPixelColor(28, color1);
+  strip.setPixelColor(41, color1);
+  strip.setPixelColor(39, color1);
+  strip.setPixelColor(38, color1);
+  strip.setPixelColor(37, color1);
+  strip.setPixelColor(35, color1);
+  strip.setPixelColor(47, color1);
+  strip.setPixelColor(46, color1);
+  strip.setPixelColor(45, color1);
+  strip.setPixelColor(44, color1);
+  strip.setPixelColor(43, color1);
+
+  strip.setPixelColor(0, color2);           //the "x"
+  strip.setPixelColor(6, color2);
+  strip.setPixelColor(12, color2);
+  strip.setPixelColor(8, color2);
+  strip.setPixelColor(18, color2);
+  strip.setPixelColor(16, color2);
+  strip.setPixelColor(24, color2);
+  strip.setPixelColor(30, color2);
+  strip.setPixelColor(36, color2);
+  strip.setPixelColor(42, color2);
+  strip.setPixelColor(48, color2);
+
+  strip.show();
+  delay(1000);                              //delay one second
+  //flip colors
+  strip.setPixelColor(1, color2);       //fill
+  strip.setPixelColor(2, color2);
+  strip.setPixelColor(3, color2);
+  strip.setPixelColor(4, color2);
+  strip.setPixelColor(5, color2);
+  strip.setPixelColor(13, color2);
+  strip.setPixelColor(11, color2);
+  strip.setPixelColor(10, color2);
+  strip.setPixelColor(9, color2);
+  strip.setPixelColor(7, color2);
+  strip.setPixelColor(20, color2);
+  strip.setPixelColor(19, color2);
+  strip.setPixelColor(17, color2);
+  strip.setPixelColor(15, color2);
+  strip.setPixelColor(14, color2);
+  strip.setPixelColor(27, color2);
+  strip.setPixelColor(26, color2);
+  strip.setPixelColor(25, color2);
+  strip.setPixelColor(23, color2);
+  strip.setPixelColor(22, color2);
+  strip.setPixelColor(21, color2);
+  strip.setPixelColor(34, color2);
+  strip.setPixelColor(33, color2);
+  strip.setPixelColor(31, color2);
+  strip.setPixelColor(29, color2);
+  strip.setPixelColor(28, color2);
+  strip.setPixelColor(41, color2);
+  strip.setPixelColor(39, color2);
+  strip.setPixelColor(38, color2);
+  strip.setPixelColor(37, color2);
+  strip.setPixelColor(35, color2);
+  strip.setPixelColor(47, color2);
+  strip.setPixelColor(46, color2);
+  strip.setPixelColor(45, color2);
+  strip.setPixelColor(44, color2);
+  strip.setPixelColor(43, color2);
+
+  strip.setPixelColor(0, color1);           //the "x"
+  strip.setPixelColor(6, color1);
+  strip.setPixelColor(12, color1);
+  strip.setPixelColor(8, color1);
+  strip.setPixelColor(18, color1);
+  strip.setPixelColor(16, color1);
+  strip.setPixelColor(24, color1);
+  strip.setPixelColor(30, color1);
+  strip.setPixelColor(36, color1);
+  strip.setPixelColor(42, color1);
+  strip.setPixelColor(48, color1);
+
+  strip.show();
+  delay(1000);                              //delay one second
+
 }
